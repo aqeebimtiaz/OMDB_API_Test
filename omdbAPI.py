3b396cbc187389ff82b80a2e0dc64be9ec4e65b3
Aqeeb Imtiaz Harun <aqeebimtiaz@gmail.com>
2019-06-28 10:30:45 +0600

    added user inputs

--- omdbAPI.py
+++ omdbAPI.py
@@ -1,14 +1,22 @@
 import requests
 import json
 
-def getMovieDetails(id):
-    url = "http://www.omdbapi.com/?i="+id+"&apikey=feb37c8d"
+def getMovieDetails(query):
+    if(query.isdigit() == False):
+        query = "t="+query
+    else:
+        query = "i="+query
+    url = "http://www.omdbapi.com/?"+query+"&apikey=feb37c8d"
     res = requests.get(url)
     # jsonData = json.dumps(res.content) 
     # data = json.loads(res)
     return res.json()["Title"]
-movieId = input('Enter movie id (IMDB id): ')
+movieId = input("Enter movie id (IMDB id): ")
 print(movieId)
-movieName = input('Enter movie name (IMDB id): ')
+movieName = input("Enter movie id (IMDB name): ")
 print(movieName)
-# print(getMovieDetails())
+if(movieName != None):
+    searchQuery = movieName
+else:
+    searchQuery = movieId
+print(getMovieDetails(searchQuery))
